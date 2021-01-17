2eaf993cec10ef3d9da0e0155f532fefb03df5b0
minaRaskovic94 <72074639+minaRaskovic94@users.noreply.github.com>
2021-01-18 00:11:43 +0100

    Async JS - HTTP requests, JSON

---
+++ src/chapter_12/http.js
@@ -0,0 +1,32 @@
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    // console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
+    } else if (request.readyState === 4) {
+      callback("could not fetch data", undefined);
+    }
+  });
+
+  request.open("GET", "todos.json"); // 2 argumenta, prvi je tip zahteva (moze biti post etc, ali u ovom slucaju je GET jer zelimo da dobijemo neki podatak); 2 argument je endpoint
+
+  request.send();
+};
+
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+  console.log("callback fired");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+console.log(3);
+console.log(4);
